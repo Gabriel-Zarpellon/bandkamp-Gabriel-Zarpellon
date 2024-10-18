d9adabe2fed44ace9defdefe198c564d3e9fdc78
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-10-18 12:57:29 -0300

    feat: remove default pagination class

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -145,10 +145,6 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 
-REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 2,
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
